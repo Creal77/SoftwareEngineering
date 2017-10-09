48ddf5a30bced083d57b01a215177a8029887924
Enzo Marioni <enzomarioni17@gmail.com>
2017-10-09 09:44:25 +0200

    Case + Plateau Création

---
+++ Demineur/Demineur/Jeu/Case.cs
@@ -0,0 +1,13 @@
+﻿using Jeu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur.Jeu
+{
+    class Case
+    {
+    }
+}
---
+++ Demineur/Demineur/Jeu/Plateau.cs
@@ -0,0 +1,41 @@
+﻿using Jeu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur.Jeu
+{
+    class Plateau
+    {
+        public Partie partie;
+        public int largeur;
+        public int hauteur;
+        Case[,] cases;
+        int mines, decouvertes, restantes;
+
+        public Plateau(Partie partie, int largeur, int longueur, int mines)
+        {
+
+        }
+
+        public Case Trouver(int x, int y)
+        {
+            return cases[x, y];
+        }
+        public void IncrementerDecouvertes()
+        {
+            decouvertes++;
+        }
+        public void ModifierMarquees(bool marquee)
+        {
+            if (marquee) restantes--; else restantes++;
+            partie.vue.ActualiserComptage(restantes);
+        }
+        public bool TesterSiGagne()
+        {
+            return decouvertes + mines == largeur * hauteur;
+        }
+    }
+}
