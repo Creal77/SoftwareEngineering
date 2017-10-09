67cac470994e8e965e8467486efdeaa5a44c3a2f
ancaranc <ancaranc@PC5403K>
2017-10-09 09:45:13 +0200

    Envoi mauvais

---
+++ Demineur/Demineur/Jeu/Plateau.cs
@@ -0,0 +1,36 @@
+﻿using Jeu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur.Jeu
+{
+    public class Plateau
+    {
+        public Partie partie;
+        public int largeur;
+        public int hauteur;
+        Case[,] cases;
+        int mines, decouvertes, restantes;
+
+        public Case Trouver(int x, int y)
+        {
+            return cases[x, y];
+        }
+        public void IncrementerDecouvertes()
+        {
+            decouvertes++;
+        }
+        public void ModifierMarquees(bool marquee)
+        {
+            if (marquee) restantes--; else restantes++;
+            partie.vue.ActualiserComptage(restantes);
+        }
+        public bool TesterSiGagne()
+        {
+            return decouvertes + mines == largeur * hauteur;
+        }
+    }
+}
